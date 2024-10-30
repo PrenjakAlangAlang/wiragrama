360771a54b946ac751001b3a690a27655d775592
PrenjakAlangAlang <luthfinurafiq76@gmail.com>
2024-10-30 09:41:59 +0700

    update home

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -52,84 +52,99 @@ class _HomeState extends State<Home> {
             Row(
               children: [
                 Expanded(
-                  flex: 1, // Lebih besar untuk membuatnya dua kali lebih lebar
-                  child: Container(
-                    padding: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: Colors.pink,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Presensi',
-                          style: TextStyle(
+                  flex: 1, // Lebar lebih besar untuk kotak Presensi
+                  child: AspectRatio(
+                    aspectRatio: 1.5, // Menentukan rasio kotak Presensi
+                    child: Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFD0772),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Presensi',
+                            style: TextStyle(
                               color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 10),
-                        Image.asset(
-                          'images/presensi.png', // Ganti dengan gambar Anda
-                          height: 50,
-                        ),
-                      ],
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment
+                                .centerRight, // Menempatkan gambar di sebelah kanan
+                            child: Image.asset(
+                              'images/presensi.png', // Ganti dengan gambar Anda
+                              height: 70,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                SizedBox(width: 10),
+                SizedBox(width: 8),
                 Expanded(
-                  flex: 1, // Ukuran lebih kecil untuk dua elemen di kanan
+                  flex: 1, // Lebar untuk kolom Kas dan Tabungan
                   child: Column(
                     children: [
-                      Container(
-                        padding: EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Kas',
-                              style: TextStyle(
+                      AspectRatio(
+                        aspectRatio: 3.2, // Menentukan rasio untuk kotak Kas
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF277513),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Kas',
+                                style: TextStyle(
                                   color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 10),
-                            Image.asset(
-                              'images/kas.png', // Ganti dengan gambar Anda
-                              height: 30,
-                            ),
-                          ],
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Image.asset(
+                                'images/kas.png', // Ganti dengan gambar Anda
+                                height: 50,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                      SizedBox(height: 10),
-                      Container(
-                        padding: EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Tabungan',
-                              style: TextStyle(
+                      SizedBox(height: 8),
+                      AspectRatio(
+                        aspectRatio:
+                            3.2, // Menentukan rasio untuk kotak Tabungan
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF0760FF),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Tabungan',
+                                style: TextStyle(
                                   color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 10),
-                            Image.asset(
-                              'images/tabungan.png', // Ganti dengan gambar Anda
-                              height: 30,
-                            ),
-                          ],
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Image.asset(
+                                'images/tabungan.png', // Ganti dengan gambar Anda
+                                height: 50,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ],
@@ -137,6 +152,7 @@ class _HomeState extends State<Home> {
                 ),
               ],
             ),
+
             SizedBox(height: 20),
             // Pengumuman Section
             Text(
@@ -159,12 +175,15 @@ class _HomeState extends State<Home> {
                   children: [
                     Text(
                       'Senin, 21 Oktober 2024',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
                     ),
                     SizedBox(height: 5),
                     Text(
                       'Pengumpulan surat izin makrab paling lambat Kamis, 24 Oktober 2024 di basecamp',
-                      style: TextStyle(color: Colors.white, fontSize: 14),
+                      style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                   ],
                 ),
@@ -184,18 +203,24 @@ class _HomeState extends State<Home> {
             Row(
               children: [
                 Expanded(
-                  child: InfoCard(
-                    title: 'Kas wiragrama',
-                    subtitle: 'Anda sudah bayar',
-                    color: const Color(0xFF3770D8),
+                  child: AspectRatio(
+                    aspectRatio: 1.5, // Tentukan rasio untuk kartu pertama
+                    child: InfoCard(
+                      title: 'Kas wiragrama',
+                      subtitle: 'Anda sudah bayar',
+                      color: const Color(0xFF3770D8),
+                    ),
                   ),
                 ),
                 SizedBox(width: 10),
                 Expanded(
-                  child: InfoCard(
-                    title: 'Presensi',
-                    subtitle: 'Anda sudah presensi',
-                    color: const Color(0xFF74819A),
+                  child: AspectRatio(
+                    aspectRatio: 1.5, // Tentukan rasio untuk kartu kedua
+                    child: InfoCard(
+                      title: 'Presensi',
+                      subtitle: 'Anda sudah presensi',
+                      color: const Color(0xFF74819A),
+                    ),
                   ),
                 ),
               ],
@@ -275,7 +300,8 @@ class InfoCard extends StatelessWidget {
           SizedBox(height: 5),
           Text(
             subtitle,
-            style: TextStyle(color: Colors.white, fontSize: 14),
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
             textAlign: TextAlign.left,
           ),
         ],
